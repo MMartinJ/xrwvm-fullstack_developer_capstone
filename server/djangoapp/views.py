65e6916da8579317226fe55f1ce397656e6ae339
MMartinJ <marina.m.jurado86@gmail.com>
2026-05-29 12:16:06 -0400

    build CarModel and CarMake Django Models task done

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,9 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
+from .models import CarMake, CarModel
+
 
 
 # Get an instance of a logger
@@ -43,6 +45,17 @@ def logout_user(request):
     logout(request) # Terminate user session
     data = {"userName":""} # Return empty username
     return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 
 # Create a `registration` view to handle sign up request
